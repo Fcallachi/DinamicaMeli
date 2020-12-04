1dc6c628b439a03196921b1169c41445cad16d4a
Weslley <weslleynoog@hotmail>
2020-12-04 17:48:26 -0300

    Agora vai rodar!

--- meLi/src/app/app.module.ts
+++ meLi/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { ScanComponent } from './scan/scan.component';
 import { PesquisaComponent } from './pesquisa/pesquisa.component';
 import { NavibarComponent } from './navibar/navibar.component';
 
@@ -13,8 +12,7 @@ import { NavibarComponent } from './navibar/navibar.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    ScanComponent,
+    LoginComponent,    
     PesquisaComponent,
     NavibarComponent
   ],
