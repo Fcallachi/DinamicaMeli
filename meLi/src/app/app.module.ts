408dd221fe7c041e362cf9b4ebe9b8d443806b5e
unknown <fernandocallachi@gmail.com>
2020-12-04 11:14:36 -0300

    implementação das rotas

--- meLi/src/app/app.module.ts
+++ meLi/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ScanComponent } from './scan/scan.component';
 import { PesquisaComponent } from './pesquisa/pesquisa.component';
+import { NavibarComponent } from './navibar/navibar.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PesquisaComponent } from './pesquisa/pesquisa.component';
     HomeComponent,
     LoginComponent,
     ScanComponent,
-    PesquisaComponent
+    PesquisaComponent,
+    NavibarComponent
   ],
   imports: [
     BrowserModule,
